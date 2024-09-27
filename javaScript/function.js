2d50e7272cb2224d275ebc04ff4412ccf41e2722
Usman Javed <usmanjaved0816@gmail.com>
2024-09-27 20:23:28 +0500

    functions

---
+++ javaScript/function.js
@@ -0,0 +1,7 @@
+function greet (Name , LastName) {
+    console.log('hello'+" " + Name + " " + LastName);
+}
+greet ('Elsa' , 'rani');
+greet ('malik' , 'saad');
+greet ('john' , 'gram');
+greet ('imran' , 'khan');
