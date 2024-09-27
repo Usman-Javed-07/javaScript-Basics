05b4708d66171506ec69d5941b383943def7e671
Usman Javed <usmanjaved0816@gmail.com>
2024-09-27 19:46:12 +0500

    arrays in js

---
+++ javaScript/arrays.js
@@ -0,0 +1,6 @@
+let SelectedColor = ['red', 'black'];
+SelectedColor[2] = 1;
+SelectedColor[3] = 'elsa';
+console.log(SelectedColor)
+console.log(SelectedColor.length)
+console.log(SelectedColor[3]);
