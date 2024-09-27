eb70768e7cf76bbeaa77923a53a3e56cb7b1d085
Usman Javed <usmanjaved0816@gmail.com>
2024-09-27 12:42:51 +0500

    variable in js

---
+++ javaScript/variable.js
@@ -0,0 +1,14 @@
+let Name = 'ali';
+console.log(Name);
+
+
+// avoid this mistakes
+// cannot be reserved keyword 
+// should be meaningful 
+// cannot start with a number (1name)
+// cannot contain a space or hypen (-)
+// Js are case Sensitive 
+
+let FirstName = 'saad';
+let LastName = 'ali';
+console.log(FirstName,LastName);
