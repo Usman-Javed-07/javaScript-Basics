49e4bbcb07eda4099d9455a57d33caefd585d8d0
Usman Javed <usmanjaved0816@gmail.com>
2024-09-27 15:23:31 +0500

    primitives types in js

---
+++ javaScript/primitives-types.js
@@ -0,0 +1,16 @@
+let Name = 'saad'; // string literal 
+let age = 24; // number 
+let isApproved = true; // boolean literal 
+let LastName = undefined; //undefined 
+let color = null; // null
+
+
+
+
+// Primitives types 
+// typeof Name
+// 'string'
+// Name = 12
+// 12
+// typeof Name
+// 'number'
